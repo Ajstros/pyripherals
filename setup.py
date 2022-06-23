ff82954a0d054b11ff446d54fa608a6369d99d6b
Ajstros <ajstros@gmail.com>
2022-06-23 15:34:05 -0500

    Add required packages to setup.py, update Test PyPI to 0.1.1

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    install_requires=[
+        "numpy>=1.21.1",
+        "numfi>=0.2.4",
+        "matplotlib>=3.5.2",
+        "pandas>=1.3.1",
+        "openpyxl>=3.0.7",
+        "h5py>=3.7.0",
+        "PyYAML>=5.4.1",
+        "scipy>=1.6.3"
+    ]
+)
