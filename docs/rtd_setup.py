47cc96503d2eee9d68caba0ccafe9dc786df4ee5
Ajstros <ajstros@gmail.com>
2022-06-24 08:11:17 -0500

    Make doc_configs a string

--- docs/rtd_setup.py
+++ docs/rtd_setup.py
@@ -8,13 +8,11 @@ import os
 home_dir = os.path.join(os.path.expanduser('~'), '.pyripherals')
 config_path = os.path.join(home_dir, 'config.yaml')
 
-doc_configs = {
-    'endpoint_max_width': 32,
-    'fpga_bitfile_path': None,
-    'ep_defines_path': None,
-    'registers_path': './python/Registers.xlsx',
-    'frontpanel_path': 'C:/Program Files/Opal Kelly/FrontPanelUSB',
-}
+doc_configs = '''endpoint_max_width: 32
+ep_defines_path: none
+fpga_bitfile_path: none
+frontpanel_path: none
+registers_path: ./python/Registers.xlsx'''
 
 print('home_dir:', home_dir)
 print('config_path:', config_path)
